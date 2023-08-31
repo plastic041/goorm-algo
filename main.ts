bc149fe4856f089d77032d00902b5f27a334b6c4
plastic041 <plastic041@gmail.com>
2023-08-31 13:12:19 +0900

    13

--- main.ts
+++ main.ts
@@ -1,84 +1,88 @@
-type Cell = 0 | 1;
+type Cell = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
 function solve(lines: string[]): number {
-  // don't use recursion
-  const size = Number(lines.shift());
+  const [size, k] = lines.shift()!.split(" ").map(Number);
   const matrix: Cell[][] = lines.map((line) =>
     line.split(" ").map((cell) => Number(cell) as Cell)
   );
 
   const visited: boolean[][] = matrix.map((row) => row.map(() => false));
 
-  let count = 0;
+  const cells = new Map<Cell, number>();
 
-  // function dfs(row: number, col: number): void {
-  //   if (row < 0 || col < 0 || row >= size || col >= size) return;
-  //   if (visited[row][col]) return;
-  //   if (matrix[row][col] === 0) return;
-
-  //   visited[row][col] = true;
-
-  //   dfs(row - 1, col);
-  //   dfs(row + 1, col);
-  //   dfs(row, col - 1);
-  //   dfs(row, col + 1);
-  // }
-
-  // don't use recursion
   for (let row = 0; row < size; row++) {
     for (let col = 0; col < size; col++) {
       const stack: [number, number][] = [];
-      if (!visited[row][col] && matrix[row][col] === 1) {
+
+      let cellCount = 0;
+      const currentCell = matrix[row][col];
+
+      if (!visited[row][col]) {
         stack.push([row, col]);
         visited[row][col] = true;
-        count++;
+        cellCount++;
       }
 
       while (stack.length > 0) {
         const [r, c] = stack.pop()!;
-        if (r - 1 >= 0 && !visited[r - 1][c] && matrix[r - 1][c] === 1) {
+        if (
+          r - 1 >= 0 && !visited[r - 1][c] && matrix[r - 1][c] === currentCell
+        ) {
           stack.push([r - 1, c]);
           visited[r - 1][c] = true;
+          cellCount++;
         }
-        if (r + 1 < size && !visited[r + 1][c] && matrix[r + 1][c] === 1) {
+        if (
+          r + 1 < size && !visited[r + 1][c] && matrix[r + 1][c] === currentCell
+        ) {
           stack.push([r + 1, c]);
           visited[r + 1][c] = true;
+          cellCount++;
         }
-        if (c - 1 >= 0 && !visited[r][c - 1] && matrix[r][c - 1] === 1) {
+        if (
+          c - 1 >= 0 && !visited[r][c - 1] && matrix[r][c - 1] === currentCell
+        ) {
           stack.push([r, c - 1]);
           visited[r][c - 1] = true;
+          cellCount++;
         }
-        if (c + 1 < size && !visited[r][c + 1] && matrix[r][c + 1] === 1) {
+        if (
+          c + 1 < size && !visited[r][c + 1] && matrix[r][c + 1] === currentCell
+        ) {
           stack.push([r, c + 1]);
           visited[r][c + 1] = true;
+          cellCount++;
         }
+      }
+
+      if (cellCount >= k) {
+        cells.set(matrix[row][col], (cells.get(matrix[row][col]) || 0) + 1);
       }
     }
   }
 
-  // for (let row = 0; row < size; row++) {
-  //   for (let col = 0; col < size; col++) {
-  //     if (!visited[row][col] && matrix[row][col] === 1) {
-  //       dfs(row, col);
-  //       count++;
-  //     }
-  //   }
-  // }
+  const sortedCells = [...cells.entries()].sort((a, b) => {
+    if (b[1] === a[1]) {
+      return b[0] - a[0];
+    }
+    return b[1] - a[1];
+  });
 
-  return count;
+  return sortedCells[0][0];
 }
 
-const data1 = `3
-0 1 0
-1 0 1
-1 1 1`;
+const data1 = `3 2
+1 1 3
+2 2 3
+3 3 2`;
 
 console.log(solve(data1.split("\n")));
 
-const data2 = `4
-1 1 1 1
-0 0 0 1
-1 1 1 1
-1 0 0 1`;
+const data2 = `5 3
+1 1 1 2 2
+3 3 3 1 2
+1 1 2 1 1
+1 2 2 2 2
+3 1 1 1 1`;
 
 console.log(solve(data2.split("\n")));
