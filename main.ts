b30f6cfbc4e5b6a2752de36f292548956a003ac3
plastic041 <plastic041@gmail.com>
2023-09-04 17:30:01 +0900

    16

--- main.ts
+++ main.ts
@@ -1,55 +1,73 @@
 function solve(lines: string[]): number {
-  const [_, money] = lines.shift()!.split(" ").map(Number);
-  const fruits = lines.map((line) => line.split(" "))
-    .map(
-      ([pStr, vStr]) => ({
-        price: Number(pStr),
-        value: Number(vStr),
-        unitValue: Number(vStr) / Number(pStr),
-      }),
-    )
-    .sort((a, b) => b.unitValue - a.unitValue);
+  const [N, _] = lines.shift()!.split(" ").map(Number);
+  const graph = new Map<number, Set<number>>();
 
-  let totalValue = 0;
-  let spent = 0;
+  for (const line of lines) {
+    const [from, to] = line.split(" ").map(Number);
+    if (!graph.has(from)) {
+      graph.set(from, new Set());
+    }
+    graph.get(from)?.add(to);
+  }
 
-  for (const fruit of fruits) {
-    const { price, value } = fruit;
+  const pairs = new Map<number, Set<number>>();
+  const visitedNodes = new Set<number>();
+  for (const node of graph.keys()) {
+    if (visitedNodes.has(node)) {
+      continue;
+    }
+    const stack = [node];
+    const visited = new Set<number>();
+    while (stack.length > 0) {
+      const current = stack.pop()!;
+      if (visited.has(current)) {
+        continue;
+      }
+      visited.add(current);
 
-    if (spent + price <= money) {
-      spent += price;
-      totalValue += value;
-    } else {
-      const availableMoney = money - spent;
-      const availableValue = availableMoney * fruit.unitValue;
-      spent += availableMoney;
-      totalValue += availableValue;
+      [...graph.get(current) || []]
+        .filter((neighbor) => !visited.has(neighbor))
+        .filter((neighbor) =>
+          graph.get(current)?.has(neighbor) && graph.get(neighbor)?.has(current)
+        )
+        .forEach((neighbor) => {
+          // console.log(neighbor, current);
+          stack.push(neighbor);
+        });
+    }
+
+    visited.delete(node);
+    if (visited.size > 0) {
+      pairs.set(node, visited);
+      visited.forEach((node) => visitedNodes.add(node));
     }
   }
 
-  return totalValue;
+  let pairsCount = 0;
+  let nodesUsedCount = 0;
+  for (const pair of pairs.values()) {
+    pairsCount++;
+    nodesUsedCount += pair.size + 1;
+  }
+  return N - nodesUsedCount + pairsCount;
 }
 
-const data1 = `6 13
-2 8
-7 35
-1 5
-3 12
-10 30
-1 7`;
+const data1 = `4 6
+2 3
+4 1
+1 2
+3 4
+1 4
+2 4`;
 
-console.log(solve(data1.split("\n")));
+console.log(solve(data1.split("\n")), 3);
 
-const data2 = `5 4
-1 999999996
-1 999999997
-1 999999998
-1 999999999
-1 1000000000`;
+const data2 = `3 6
+1 2
+1 3
+2 1
+2 3
+3 1
+3 2`;
 
-console.log(solve(data2.split("\n")));
-
-const data3 = `1 1
-1000000000 1000000000`;
-
-console.log(solve(data3.split("\n")));
+console.log(solve(data2.split("\n")), 1);
