68245c51a1b59245cb0a0b0411fa77ce4a8e30f8
plastic041 <plastic041@gmail.com>
2023-09-06 12:14:55 +0900

    18

--- main.ts
+++ main.ts
@@ -1,116 +1,85 @@
-function solve(lines: string[]): string {
-  const [pcCount, edgeCount] = lines.shift()!.split(" ").map(Number);
-  const map = new Map<number, Set<number>>();
-  lines
-    .map((line) => line.split(" ").map(Number))
-    .forEach(([from, to]) => {
-      if (!map.has(from)) {
-        map.set(from, new Set());
+function solve(lines: string[]): number {
+  const [size, rayCount] = lines.shift()!.split(" ").map(Number);
+
+  type Direction = "R" | "L" | "U" | "D";
+
+  type Cell = {
+    horizontalRayCount: number;
+    verticalRayCount: number;
+  };
+
+  type Coord = {
+    x: number;
+    y: number;
+  };
+
+  type RayDirection = "horizontal" | "vertical";
+
+  function getCrossoverCount(cell: Cell): number {
+    return cell.horizontalRayCount * cell.verticalRayCount;
+  }
+
+  function getCell(cells: Cell[], coord: Coord): Cell {
+    const { x, y } = coord;
+
+    return cells[x + y * size];
+  }
+
+  function addRayToCell(
+    cells: Cell[],
+    coord: Coord,
+    direction: RayDirection,
+  ): Cell {
+    const cell = getCell(cells, coord);
+    cell[`${direction}RayCount`]++;
+
+    return cell;
+  }
+
+  const cells = new Array(size * size).fill(0).map(() => ({
+    horizontalRayCount: 0,
+    verticalRayCount: 0,
+  }));
+  lines.forEach((line) => {
+    const [y, x, direction] = line.split(" ");
+    const coord = { x: Number(x) - 1, y: Number(y) - 1 };
+
+    if (direction === "R") {
+      for (let i = coord.x; i < size; i++) {
+        addRayToCell(cells, { x: i, y: coord.y }, "horizontal");
       }
-      map.set(from, map.get(from)!.add(to));
-      if (!map.has(to)) {
-        map.set(to, new Set());
+    } else if (direction === "L") {
+      for (let i = coord.x; i >= 0; i--) {
+        addRayToCell(cells, { x: i, y: coord.y }, "horizontal");
       }
-      map.set(to, map.get(to)!.add(from));
-    });
-
-  const clusters: Array<Array<number>> = [];
-  const visited = new Set<number>();
-
-  map.forEach((_tos, from) => {
-    if (!visited.has(from)) {
-      const cluster: Array<number> = [];
-
-      const queue = [from];
-      while (queue.length > 0) {
-        const current = queue.shift()!;
-        if (!visited.has(current)) {
-          visited.add(current);
-          cluster.push(current);
-          map.get(current)?.forEach((to) => {
-            if (!visited.has(to)) {
-              queue.push(to);
-            }
-          });
-        }
+    } else if (direction === "U") {
+      for (let i = coord.y; i >= 0; i--) {
+        addRayToCell(cells, { x: coord.x, y: i }, "vertical");
       }
-
-      clusters.push(cluster);
+    } else if (direction === "D") {
+      for (let i = coord.y; i < size; i++) {
+        addRayToCell(cells, { x: coord.x, y: i }, "vertical");
+      }
     }
   });
 
-  function calculateDensity(cluster: Array<number>) {
-    if (cluster.length === 0) throw new Error("cluster size is 0");
-    const edgesCount = [...cluster].reduce(
-      (count, from) => count + map.get(from)!.size,
-      0,
-    ) / 2;
-
-    return edgesCount / cluster.length;
-  }
-
-  function getResultString(cluster: Array<number>) {
-    return `${[...cluster].sort((a, b) => a - b).join(" ")} `;
-  }
-
-  const densities: Map<Array<number>, number> = clusters.reduce(
-    (map, cluster) => map.set(cluster, calculateDensity(cluster)),
-    new Map(),
-  );
-  const highestDensity = Math.max(...densities.values());
-
-  const clustersWithHighestDensity = clusters
-    .filter((cluster) => densities.get(cluster) === highestDensity);
-
-  if (clustersWithHighestDensity.length === 1) {
-    return getResultString(clustersWithHighestDensity[0]);
-  }
-
-  const lowestSize = Math.min(
-    ...clustersWithHighestDensity.map((cluster) => cluster.length),
-  );
-  const clustersWithLowestSize = clustersWithHighestDensity.filter(
-    (cluster) => cluster.length === lowestSize,
-  );
-
-  if (clustersWithLowestSize.length === 1) {
-    return getResultString(clustersWithLowestSize[0]);
-  }
-
-  const lowestValue = Math.min(
-    ...clustersWithLowestSize.map((cluster) => Math.min(...cluster)),
-  );
-  const clustersWithLowestValue = clustersWithHighestDensity.find(
-    (cluster) => cluster.includes(lowestValue),
-  )!;
-
-  return getResultString(clustersWithLowestValue);
+  return cells.reduce((acc, cell) => {
+    return acc + getCrossoverCount(cell);
+  }, 0);
 }
 
-const data1 = `7 6
-1 3
-5 3
-3 7
-7 1
-2 4
-4 6`;
+const data1 = `3 5
+2 1 R
+1 1 D
+2 3 L
+3 3 U
+2 2 D`;
 
-console.log(solve(data1.split("\n")), "1 3 5 7 ");
+console.log(solve(data1.split("\n")), 6);
 
-const data2 = `6 6
-2 3
-5 3
-2 6
-1 2
-1 4
-5 4`;
+const data2 = `3 3
+2 2 R
+2 2 L
+1 2 D`;
 
-console.log(solve(data2.split("\n")), "1 2 3 4 5 6 ");
-
-const data3 = `17 4
-16 17
-3 16
-1 17
-7 5`;
-
-console.log(solve(data3.split("\n")), "1 3 16 17 ");
+console.log(solve(data2.split("\n")), 2);
