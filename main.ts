11bb2bd9c03a9a366c755bb87d1820a94bc4ace1
plastic041 <plastic041@gmail.com>
2023-09-07 17:33:30 +0900

    19

--- main.ts
+++ main.ts
@@ -1,85 +1,115 @@
-function solve(lines: string[]): number {
-  const [size, rayCount] = lines.shift()!.split(" ").map(Number);
+function solve(lines: string[]): string {
+  type Node = number;
 
-  type Direction = "R" | "L" | "U" | "D";
-
-  type Cell = {
-    horizontalRayCount: number;
-    verticalRayCount: number;
-  };
-
-  type Coord = {
-    x: number;
-    y: number;
-  };
-
-  type RayDirection = "horizontal" | "vertical";
-
-  function getCrossoverCount(cell: Cell): number {
-    return cell.horizontalRayCount * cell.verticalRayCount;
-  }
-
-  function getCell(cells: Cell[], coord: Coord): Cell {
-    const { x, y } = coord;
-
-    return cells[x + y * size];
-  }
-
-  function addRayToCell(
-    cells: Cell[],
-    coord: Coord,
-    direction: RayDirection,
-  ): Cell {
-    const cell = getCell(cells, coord);
-    cell[`${direction}RayCount`]++;
-
-    return cell;
-  }
-
-  const cells = new Array(size * size).fill(0).map(() => ({
-    horizontalRayCount: 0,
-    verticalRayCount: 0,
-  }));
+  const [N, M, START, END] = lines.shift()!.split(" ").map(Number);
+  const graphFrom: Map<Node, Set<Node>> = new Map();
+  const graphTo: Map<Node, Set<Node>> = new Map();
   lines.forEach((line) => {
-    const [y, x, direction] = line.split(" ");
-    const coord = { x: Number(x) - 1, y: Number(y) - 1 };
-
-    if (direction === "R") {
-      for (let i = coord.x; i < size; i++) {
-        addRayToCell(cells, { x: i, y: coord.y }, "horizontal");
-      }
-    } else if (direction === "L") {
-      for (let i = coord.x; i >= 0; i--) {
-        addRayToCell(cells, { x: i, y: coord.y }, "horizontal");
-      }
-    } else if (direction === "U") {
-      for (let i = coord.y; i >= 0; i--) {
-        addRayToCell(cells, { x: coord.x, y: i }, "vertical");
-      }
-    } else if (direction === "D") {
-      for (let i = coord.y; i < size; i++) {
-        addRayToCell(cells, { x: coord.x, y: i }, "vertical");
-      }
+    const [from, to] = line.split(" ").map(Number);
+    if (!graphFrom.has(from)) {
+      graphFrom.set(from, new Set());
     }
+    graphFrom.get(from)?.add(to);
+    if (!graphTo.has(to)) {
+      graphTo.set(to, new Set());
+    }
+    graphTo.get(to)?.add(from);
   });
 
-  return cells.reduce((acc, cell) => {
-    return acc + getCrossoverCount(cell);
-  }, 0);
+  function dfs(disabled: number) {
+    const queue: Array<[Node, number]> = [[START, 1]];
+    const visited: Set<Node> = new Set();
+
+    if (disabled === START || disabled === END) {
+      return -1;
+    }
+
+    while (queue.length > 0) {
+      const [node, steps] = queue.shift()!;
+      if (node === END) {
+        return steps;
+      }
+      visited.add(node);
+      const nextNodesFrom = graphFrom.get(node);
+      const nextNodesTo = graphTo.get(node);
+      if (nextNodesFrom) {
+        nextNodesFrom.forEach((nextNode) => {
+          if (nextNode !== disabled && !visited.has(nextNode)) {
+            queue.push([nextNode, steps + 1]);
+          }
+        });
+      }
+      if (nextNodesTo) {
+        nextNodesTo.forEach((nextNode) => {
+          if (nextNode !== disabled && !visited.has(nextNode)) {
+            queue.push([nextNode, steps + 1]);
+          }
+        });
+      }
+    }
+    return -1;
+  }
+
+  let result = "";
+  for (let i = 1; i <= N; i++) {
+    result += `${dfs(i)}
+`;
+  }
+
+  return result;
 }
 
-const data1 = `3 5
-2 1 R
-1 1 D
-2 3 L
-3 3 U
-2 2 D`;
+const data1 = `5 5 1 4
+1 3
+4 3
+2 5
+4 2
+1 5`;
 
-console.log(solve(data1.split("\n")), 6);
+console.log(
+  solve(data1.split("\n")),
+  `-1
+3
+4
+-1
+3`,
+);
 
-const data2 = `3 3
-2 2 R
-2 2 L
-1 2 D`;
+// const data2 = `4 4 3 1
+// 4 1
+// 4 3
+// 3 2
+// 2 1`;
 
-console.log(solve(data2.split("\n")), 2);
+// console.log(
+//   solve(data2.split("\n")),
+//   `-1
+// 3
+// -1
+// 3`,
+// );
+
+// const data3 = `9 10 1 9
+// 1 2
+// 1 3
+// 3 4
+// 2 5
+// 4 5
+// 5 6
+// 6 7
+// 5 8
+// 7 9
+// 8 9`;
+
+// console.log(
+//   solve(data3.split("\n")),
+//   `-1
+// 6
+// 5
+// 5
+// -1
+// 5
+// 5
+// 6
+// -1`,
+// );
